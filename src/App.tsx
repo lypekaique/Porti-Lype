c90c74e2b586ec3df56480d0f851c1ac63264b8d
lypekaique <lypekaique@hotmail.com>
2024-09-19 05:25:14 -0300

    css Propriedades Comuns

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ export default function App() {
       <header class="flex justify-between items-center p-0 bg-[#10E956] z-100 w-full h-[10vh] sticky top-0 animate-fade-down animate-once animate-ease-in-out animate-normal animate-fill-forwards">
         <img class='animate-fade-down animate-once animate-ease-in-out animate-normal animate-fill-forwards w-[54px] h-[54px] fixed p-0 z-[500] left-[4rem] top-[--7px]'  src={logo}/>
       </header>
-      <div class=''>
-        <div class=''></div>
-      </div>
+      <div></div>
 
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wobble: 'wobble 2.5s infinite',
+      },
+      colors: {
+        'custom-green': '#14E956',
+      },
+      fontFamily: {
+        cyborg: ['Cyborg'],
+      },
+      boxShadow: {
+        custom: '3px 3px 0 #1F2127, -1px -1px 0 #1F2127, 1px -1px 0 #1F2127, -1px 1px 0 #1F2127, 1px 1px 0 #1F2127',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-animated')
