23898c549e2707ee93707a71a2cddc57db7fdbac
lypekaique <lypekaique@hotmail.com>
2024-09-19 05:00:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from '../src/assets/IMG/logo.7eb7df78c9c3f0b293b578bb70239387.svg';
+
+
+export default function App() {
+  return (
+    <div>
+      <header class="flex justify-between items-center p-0 bg-[#10E956] z-100 w-full h-[10vh] sticky top-0 animate-fade-down animate-once animate-ease-in-out animate-normal animate-fill-forwards">
+        <img class='animate-fade-down animate-once animate-ease-in-out animate-normal animate-fill-forwards w-[54px] h-[54px] fixed p-0 z-[500] left-[4rem] top-[--7px]'  src={logo}/>
+      </header>
+      <div class=''>
+        <div class=''></div>
+      </div>
+
+    </div>
+  )
+}
